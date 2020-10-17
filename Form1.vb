3957f096bbd41f877795324ef486094f0410ce2d
RayChau <ray23chau@gmail.com>
2020-10-17 22:35:21 +0800

    Update Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -11,8 +11,8 @@
     Dim mousey As Integer
 
     Private Sub frmModuleGradeCalculator_Load(sender As Object, e As EventArgs) Handles MyBase.Load 'Actions when the program started
-        lblCAComponents.Text = "CA Components: Test:" & Test_percentage * 100 & "%, Quiz:" & Quiz_percentage * 100 & ", Project:" _
-                                & Project_percentage * 100 'Display CA components
+        lblCAComponents.Text = "CA Components: Test:" & Test_percentage * 100 & "%, Quiz:" & Quiz_percentage * 100 & "%, Project:" _
+                                & Project_percentage * 100 & "%" 'Display CA components
         grpResult.Text = "Module Result (CA:" & CA_percentage * 100 & "%, Exam:" & Exam_percentage * 100 & "%)" 'Add module components to groupbox
     End Sub
 
@@ -20,81 +20,32 @@
 
         Dim name As String
         Dim test, quiz, project, exam, camrak, modulemark As Double
-        Dim valid_input_count As Integer
+        Dim valid_input As Integer '5 = invalid name 4 = valid 0-3 = invalid numbers
 
-        'test = CDbl(txtTest.Text)
-        'quiz = CDbl(txtQuizzes.Text)
-        'project = CDbl(txtProject.Text)
-        'exam = CDbl(txtExam.Text)
-
-        'if (txttest.text = "" or test < 0 or test > 100 or not (isnumeric(txttest.text))) then
-        '    if txttest.text = "" then
-        '        valid = 1
-        '    elseif not (isnumeric(txttest.text)) then
-        '        valid = 2
-        '    else
-        '        valid = 3
-        '    end if
-        'elseif (txtquizzes.text = "" or test < 0 or test > 100 or not (isnumeric(txtquizzes.text))) then
-        '    if txtquizzes.text = "" then
-        '        valid = 1
-        '    elseif not (isnumeric(txtquizzes.text)) then
-        '        valid = 2
-        '    else
-        '        valid = 3
-        '    end if
-        'elseif (txtproject.text = "" or test < 0 or test > 100 or not (isnumeric(txtproject.text))) then
-        '    if txtproject.text = "" then
-        '        valid = 1
-        '    elseif not (isnumeric(txtproject.text)) then
-        '        valid = 2
-        '    else
-        '        valid = 3
-        '    end if
-        'elseif (txtexam.text = "" or test < 0 or test > 100 or not (isnumeric(txtexam.text))) then
-        '    if txtexam.text = "" then
-        '        valid = 1
-        '    elseif not (isnumeric(txtexam.text)) then
-        '        valid = 2
-        '    else
-        '        valid = 3
-        '    end if
-        'end if
-
-        'If valid = 1 Then
-        '    MessageBox.Show("Input cannot be empty")
-        'ElseIf valid = 2 Then
-        '    MessageBox.Show("Please Enter a Number")
-        'ElseIf valid = 3 Then
-        '    MessageBox.Show("Please Enter marks in between 0 - 100")
-        'End If
-
-        If (IsNumeric(txtTest.Text)) And (IsNumeric(txtQuizzes.Text)) And (IsNumeric(txtProject.Text)) And (IsNumeric(txtExam.Text)) Then
+        If (txtName.Text = "") Or IsNumeric(txtName.Text) Then 'Check student name which is cannot be empty and a number
+            MessageBox.Show("Please Enter Student Name", "Invalid Input")
+            valid_input = 5
+        ElseIf (IsNumeric(txtTest.Text)) And (IsNumeric(txtQuizzes.Text)) And (IsNumeric(txtProject.Text)) And (IsNumeric(txtExam.Text)) Then 'Check marks input  
             name = txtName.Text 'Input Name
             test = CDbl(txtTest.Text)
-            valid_input_count = Validate_marks(test)
+            valid_input = Validate_marks(test) 'count numbers of valid marks input
             quiz = CDbl(txtQuizzes.Text) 'Input Quizzes mark
-            valid_input_count = valid_input_count + Validate_marks(quiz)
+            valid_input = valid_input + Validate_marks(quiz)
             project = CDbl(txtProject.Text) 'Input Project mark
-            valid_input_count = valid_input_count + Validate_marks(project)
+            valid_input = valid_input + Validate_marks(project)
             exam = CDbl(txtExam.Text) 'Input Exam mark
-            valid_input_count = valid_input_count + Validate_marks(exam)
+            valid_input = valid_input + Validate_marks(exam)
         Else
             MessageBox.Show("Please Enter a number", "Invalid Input")
+            valid_input = 5 'to prevent error message display wrongly
             Clear_Output()
         End If
 
-        If valid_input_count = 4 Then
+        If valid_input = 4 Then 'If there are 4 valid input, calculate the result
             camrak = test * Test_percentage + quiz * Quiz_percentage + project * Project_percentage
             modulemark = camrak * CA_percentage + exam * Exam_percentage
             txtCA.Text = CStr(camrak)
             txtModule.Text = CStr(modulemark)
-        Else
-            MessageBox.Show("Please Enter marks in between 0 - 100", "Invalid Input")
-            Clear_Output()
-        End If
-
-        If (valid_input_count = 4) Then
             If ((camrak < 40) Or (exam < 40)) Then 'Determine Grade
                 txtGrade.Text = "F"
             ElseIf modulemark < 65 Then
@@ -104,9 +55,11 @@
             Else
                 txtGrade.Text = "A"
             End If
-        Else
+        ElseIf valid_input < 4 Then 'If there are any invalid numbers, display error message 
+            MessageBox.Show("Please Enter marks in between 0 - 100", "Invalid Input")
             Clear_Output()
         End If
+
     End Sub
 
     Private Sub btnClearAll_Click(sender As Object, e As EventArgs) Handles btnClearAll.Click 'Clear everything in Marks and Grade for Individual Student groupbox
@@ -155,21 +108,12 @@
         txtRemarks.Text = ""
     End Sub
 
-    'Function Check_empty(ByVal input As String) As String
-    '    If input = "" Then
-    '        MessageBox.Show("Input Cannot Be Empty", "Error")
-    '    End If
-    '    Return input
-    'End Function
-
-    Function Validate_marks(ByVal mark As Double) As Integer
+    Function Validate_marks(ByVal mark As Double) As Integer 'validate marks input
         If mark >= 0 And mark <= 100 Then
             Return 1
         Else
             Return 0
         End If
     End Function
-
-
 
 End Class
